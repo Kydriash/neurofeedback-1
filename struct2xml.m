3036a18389fedb1f2af90a71874a2994066d4afc
an-zelenskaya <an.zelenskaya@gmail.com>
2015-05-29 11:29:23 +0300

    xml output fixed

--- struct2xml.m
+++ struct2xml.m
@@ -8,7 +8,8 @@ function xml = struct2xml(v)
 xml = [];
 
 % New line character:
-nl = '';%char(13);
+nl = char(13);
+sp = '';
 % Use nl = '' to remove new line characters.
 
 names = fieldnames(v);
@@ -27,11 +28,11 @@ for n = 1:length(names)
         if iscell(a); Nitems = length(a); else Nitems=1; a={a};end
         
         for i = 1:Nitems
-            xml = [xml '<' names{n} '>' nl];
+            xml = [xml '<' names{n} '>' sp];
             if ischar(a{i})
-                xml = [xml a{i} nl];
+                xml = [xml a{i} sp];
             else
-                xml = [xml num2str(a{i}') nl];
+                xml = [xml num2str(a{i}') sp];
             end
             xml = [xml '</' names{n} '>' nl];
         end
